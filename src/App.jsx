0626ddec792e5d26364ef642b0a3f059e3586b8c
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-11-13 13:04:46 +0530

    navbar component added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar/> 
+    </div>
+  )
+}
+
+export default App
