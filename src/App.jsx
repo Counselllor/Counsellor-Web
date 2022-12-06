bae428206ee75004ff65a6ba287d292898faf9e0
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-12-07 01:35:29 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/App.css'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
+import SignUp from './components/SignUp'
 
-function App() {
+const App = () => {
 
   return (
-    <div className="App">
-      <Navbar/> 
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar/> 
+      </div>
+
+      <Routes>
+        <Route exact path='/' element={<Home/>}></Route>
+        <Route exact path='/about' element={<About/>}></Route>
+        <Route exact path='/login' element={<Login/>}></Route>  
+        <Route exact path='/signup' element={<SignUp/>}></Route>  
+      </Routes>
+    </Router>
   )
 }
 
