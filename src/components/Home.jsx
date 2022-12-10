dad892a57ccd4be00a6b1536bfb7369038b8a614
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-12-10 17:56:44 +0530

    removed redundant div

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,10 +3,10 @@ import '../styles/App.css'
 
 const Home = () => {
     return(
-        <div>
+        <>
             <img id='meetingImg' src={MeetingPhoto} alt="Meeting Image" />
             <p id='homeTxt'>Still Confused with College Choice?</p>
-        </div>
+        </>
     )
 }
 
