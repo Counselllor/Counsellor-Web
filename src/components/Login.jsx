b2b8de9d48084aa5f894596b8e0015782c9eca47
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-12-12 22:34:58 +0530

    fixed a bug

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,7 @@
-import firstShape from '../assets/shape1.png'
-import secondShape from '../assets/shape2.png'
-
 const Login = () => {
     return(
         <>
-            
+
         </>
     )
 }
