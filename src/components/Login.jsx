e5af2122285bda7f70ff0ca42782633d089537a4
LinuxDecoded <ajshrmaofficial@gmail.com>
2022-12-12 08:45:27 +0530

    finalised changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,11 @@
+import firstShape from '../assets/shape1.png'
+import secondShape from '../assets/shape2.png'
+
 const Login = () => {
     return(
-        <div>
-            login
-        </div>
+        <>
+            
+        </>
     )
 }
 
