7c1a55a296a6a03cd1a5a50eae34c5bc5535b934
Thestarsahil Github <sahilali88827@gmail.com>
2023-02-19 20:40:12 +0530

    signup update

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import ElementPhoto from '../assets/element1.png'
 import Logo from '../assets/logo.png'
+import GoogleLogo from '../assets/googleicon.png'
+import WindowLogo from '../assets/windowicon.png'
+import Githublogo from '../assets/githubicon2.png'
 import ElementPhoto4 from '../assets/element 4.png'
 
 const LoginForm = () => {
@@ -18,19 +21,23 @@ const LoginForm = () => {
 
         {/* This is the left side of the login page   */}
         <div className="left-side">
-        <div className="left-side-logo">
-          <img id='logo' src={Logo} alt="logo" />
-          <p id='logintxt_leftside' >For the Students By the Students</p>
-        </div>
-        <img id='element1Img' src={ElementPhoto} alt="Element Image" />
+          <div className="left-side-logo">
+            <img id='logo' src={Logo} alt="logo" />
+            <p id='logintxt_leftside' >For the Students By the Students</p>
+          </div>
+          <img id='element1Img' src={ElementPhoto} alt="Element Image" />
         </div>
 
 
         {/* This is the right side of the login page   */}
         <div className="right-side">
-        <div className="counsellor-text-container">Counsellor</div>
-        <div className='lets-signin-text-container'>Let's You Sign In!</div>
-        <div className='needhelp-container'>Need Help?</div>
+          <div className="counsellor-text-container">Counsellor</div>
+          <div className="lets-signin-text-container">Let's You Sign In!</div>
+          <div className='needhelp-container'>Need Help?</div>
+
+          {/* Login with */}
+
+
           <form onSubmit={handleSubmit}>
             <input
               type="text"
@@ -45,6 +52,14 @@ const LoginForm = () => {
               placeholder="Password"
             />
             <button type="submit">Login</button>
+            <div className='loginOptions'>
+              <div classname="or-login-with">Or login with </div>
+              <div classname="login-with-icons">
+                <img id='googleicon' src={GoogleLogo} alt="gogoleicon" />
+                <img id='windowicon' src={WindowLogo} alt="windowicon" />
+                < img id="githubicon" src={Githublogo} alt="githubicon" />
+              </div>
+            </div>
           </form>
         </div>
       </div>
--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,7 +1,16 @@
 const SignUp = () => {
-    return(
-        <div>
-            signup
+    return (
+        <div className="signup-container">
+            <div className="shadow-box">
+                <div class="parent">
+                    <div className="signuptxt">Sign up </div>
+                    <div className="signuptxt2">It's quick and easy</div>
+                </div>
+                <div className="signup-line"></div>
+                
+
+
+            </div>
         </div>
     )
 }
