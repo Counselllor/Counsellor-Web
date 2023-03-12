d48b6658e00bbb605dec29a231cce215d254c813
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-03-12 18:41:11 +0530

    added new Error Page

---
+++ src/components/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import {Link, useRouterError} from 'react-router-dom'
+import '../styles/App.css'
+
+const ErrorPage = () => {
+    const error = useRouterError()
+    
+    return(
+        <div className="ErrorPage">
+            <h1>Oops! Something went wrong</h1>
+            <p>Sorry, an unexpected error has occured.</p>
+            <p>
+                <i>
+                    {error?.statusText || error?.message}
+                </i>
+            </p>
+            <Link to={'/'}>Go back to home</Link>
+        </div>
+    )
+}
+
+export default ErrorPage
