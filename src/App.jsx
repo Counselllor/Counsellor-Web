366e7cc7a562911505aa700bc311861a8b15f97d
Manisha Swain <manishaswain2003@gmail.com>
2023-05-29 17:58:09 +0530

    added pre loader page issue 79

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import { Outlet } from "react-router-dom";
 import "./styles/App.css";
 import Navbar from "./components/Navbar";
+import React, { useState, useEffect } from "react";
 
+import HashLoader from "react-spinners/HashLoader";
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
   return (
-    <div className="App">
-      <Navbar />
-      <Outlet />
+    <div>
+      {loading ? (
+        <div className="loader ">
+          <HashLoader
+            loading={loading}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+            color={"#0603a9"}
+          />
+        </div>
+      ) : (
+        <div className="App">
+          <Navbar />
+          <Outlet />
+        </div>
+      )}
     </div>
   );
 };
