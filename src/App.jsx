bc55052e8c1ed6f5ae90e03ca3fd711c157829be
ANSHIKA JAIN <anitajain0252@gmail.com>
2023-05-30 12:24:53 +0530

    Typewriter effect done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { Outlet } from "react-router-dom";
 import "./styles/App.css";
 import Navbar from "./components/Navbar";
+import React from 'react';
+import Typewriter from 'typewriter-effect';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Navbar />
-      <Outlet />
-    </div>
-  );
-};
+const App = () =>{
+return (
+	<div className="App">
+  <Navbar />
+  <Outlet />
+  <div className="MainText">
+	<Typewriter
+	  onInit={(typewriter)=> {
+	  typewriter.typeString("Still Confused with College Choice?")
+	  .pauseFor(10)
+	  .start();
+	}}
+	/>
+  </div>
+	</div>
+);
+}
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,6 @@ const Home = () => {
             <div id='homeCircle'></div>
             <div id='homeContainer'>
                 <img id='meetingImg' src={MeetingPhoto} alt="Meeting Image" />
-                <p id='homeTxt'>Still Confused with College Choice?</p>
             </div>
             <div id='homeCircleScnd'></div>
         </>
