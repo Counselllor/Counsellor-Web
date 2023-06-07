fc0e36c259d8a3eef2f3b55aa99d4d1611a445de
Sumit Kumar Rai <100675296+sumitkr2000@users.noreply.github.com>
2023-06-07 22:03:07 +0530

    Improved UI of signup and login page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,24 +1,28 @@
-import {Link, useRouteError} from 'react-router-dom'
+import { Link, useRouteError } from "react-router-dom";
 // import '../styles/App.css'
-import './ErrorPage.css'
-import img from '../../assets/error404.svg';
+import "./ErrorPage.css";
+import img from "../../assets/error404.svg";
+import Navbar from "../Navbar/Navbar";
 
 const ErrorPage = () => {
-    const error = useRouteError()
-    
-    return(
-        <div className="ErrorPage">
-            <h1>Oops! Something went wrong</h1>
-            <img className='Image' src={img} alt='404'></img>
-            <p>Sorry, an unexpected error has occured.</p>
-            <p>
-                <i>
-                    {error?.statusText || error?.message}
-                </i>
-            </p>
-            <Link className='Btn' to={'/'}>Go back to home</Link>
-        </div>
-    )
-}
+  const error = useRouteError();
 
-export default ErrorPage
+  return (
+    <>
+    <Navbar />
+      <div className="ErrorPage">
+        <h1>Oops! Something went wrong</h1>
+        <img className="Image" src={img} alt="404"></img>
+        <p>Sorry, an unexpected error has occured.</p>
+        <p>
+          <i>{error?.statusText || error?.message}</i>
+        </p>
+        <Link className="Btn" to={"/"}>
+          Go back to home
+        </Link>
+      </div>
+    </>
+  );
+};
+
+export default ErrorPage;
