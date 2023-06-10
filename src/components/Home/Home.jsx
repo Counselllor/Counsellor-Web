d1d36cf34c58f03df101ec5b88c6c5c25af4adc6
Shubham Ingole <90132010+Shubham7350@users.noreply.github.com>
2023-06-10 09:38:21 +0530

    react-parallex-tilt added for meeting image

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,28 +1,31 @@
-import MeetingPhoto from '../../assets/meeting.webp'
-import './Home.css'
-import React from 'react';
-import Typewriter from 'typewriter-effect';
+import MeetingPhoto from "../../assets/meeting3.png";
+import "./Home.css";
+import React from "react";
+import Typewriter from "typewriter-effect";
+import Tilt from "react-parallax-tilt";
 
 const Home = () => {
-    return(
-        
-        <>
-            <div id='homeCircle'></div>
-            <div id='homeContainer'>
-                <img id='meetingImg' src={MeetingPhoto} alt="Meeting Image" />  
-                <div className="MainText">
-                    <Typewriter
-                        onInit={(typewriter) => {
-                            typewriter.typeString("Still Confused with College Choice?")
-                                .pauseFor(10)
-                                .start();
-                        }}
-                    />
-                </div>
-            </div>
-            <div id='homeCircleScnd'></div>
-        </>
-    )
-}
+  return (
+    <>
+      <div id="homeCircle"></div>
+      <div id="homeContainer">
+        <Tilt className="tiltImg">
+          <img id="meetingImg" src={MeetingPhoto} alt="Meeting Image" />
+        </Tilt>
+        <div className="MainText">
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Still Confused with College Choice?")
+                .pauseFor(10)
+                .start();
+            }}
+          />
+        </div>
+      </div>
+      <div id="homeCircleScnd"></div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
