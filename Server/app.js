8eb85890b17522fa50a6ce1edc308d9aa6d3ca23
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-05-12 14:43:19 +0530

    Feture-> rate limit added, 100 request per minute per IP

--- Server/app.js
+++ Server/app.js
@@ -1,17 +1,24 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit');
 const app=express()
 const mongoose =require('mongoose')
 const PORT =4000
 const {MONGOURI}=require("./Key")
 
 require('./models/User')
-
+const limit = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 100, // Limit each IP to 100 requests per window
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 app.use(express.json())
 app.use(require('./routes/Auth'))
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:4000');
     next();
   });
+app.use(limit);
 // app.use(cors({
 //     origin:"http://localhost:4000"
 // }))
