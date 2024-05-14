cd4fd8f96227d2b13182df3e2e0467ffa981f3e5
Pranali3103 <pranali312003@gmail.com>
2024-05-14 23:36:50 +0530

    Changed Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./styles/App.css";
-
+import Footer from "./components/Footer/Footer";
 const App = () => {
 
   return (
     <div className="App">
       <Outlet />
+      {/* <Footer/> */}
     </div>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,87 @@
+// import "./Footer.css";
+// import { FaGithub, FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
+
+// function Footer() {
+//   return (
+//     <footer>
+//       <div>
+//         <ul>
+//           <li>
+//             <a
+//               style={{ textDecoration: "none", color: "inherit" }}
+//               href="./about"
+//             >
+//               About
+//             </a>
+//           </li>
+//           <li>Blog</li>
+//           <li>Help</li>
+//           <li>Privacy</li>
+//           <li>Terms</li>
+//           <li>
+//             <a
+//               style={{ textDecoration: "none", color: "inherit" }}
+//               href="./contact"
+//             >
+//               Contact
+//             </a>
+//           </li>
+//           <li>Our Countributors</li>
+//           <li>Join Us</li>
+//         </ul>
+//         <p>© 2023 Counsellor</p>
+//         <div id="icons">
+//           <a
+//             href="https://github.com/Counselllor/Counsellor-Web"
+//             target="_blank"
+//           >
+//             <FaGithub />
+//           </a>
+//           <FaFacebook />
+//           <FaInstagram />
+//           <FaLinkedin />
+//         </div>
+//       </div>
+//     </footer>
+//   );
+// }
+
+// export default Footer;
+
+
 import "./Footer.css";
 import { FaGithub, FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
   return (
-    <footer>
-      <div>
-        <ul>
-          <li>
-            <a
-              style={{ textDecoration: "none", color: "inherit" }}
-              href="./about"
-            >
-              About
-            </a>
-          </li>
+    <div className="footer">
+      <footer>
+           
+  <div style={{ textAlign: 'left' , paddingLeft:'20px'}}>
+  <span style={{ fontSize: '100px' }}>Counsellor.</span>
+  </div>
+<div className="footer-container">
+        <ul >
+          <li>Home</li>
+          <li>About</li>
           <li>Blog</li>
           <li>Help</li>
           <li>Privacy</li>
-          <li>Terms</li>
-          <li>
-            <a
-              style={{ textDecoration: "none", color: "inherit" }}
-              href="./contact"
-            >
-              Contact
-            </a>
+        <li>Terms</li>
+
+        <li>
+        <a
+        style={{ textDecoration: "none", color: "inherit" }}
+     href="./contact"
+    >             Contact
+           </a>
           </li>
-          <li>Our Countributors</li>
-          <li>Join Us</li>
         </ul>
-        <p>© 2023 Counsellor</p>
-        <div id="icons">
-          <a
-            href="https://github.com/Counselllor/Counsellor-Web"
-            target="_blank"
-          >
-            <FaGithub />
-          </a>
-          <FaFacebook />
-          <FaInstagram />
-          <FaLinkedin />
-        </div>
-      </div>
-    </footer>
+       </div>
+
+
+      </footer>
+    </div>
   );
 }
 
