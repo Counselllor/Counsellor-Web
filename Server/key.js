9876696e0439cca691dd9515dc769b8758af3861
Sahil Ali <84167034+thestarsahil@users.noreply.github.com>
2024-05-14 07:56:13 +0000

    server file deleted

--- Server/key.js
+++
@@ -1,6 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://VidushiGandhi:YhbuK3ov4gZHlJxG@cluster0.yzbdbwj.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"gdjhvvdhjdbvxvjLsasdhpdbcghcjcjjhjhsbshshsbwey"
-
-}
-
