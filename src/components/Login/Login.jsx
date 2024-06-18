cb4f25411af8a6d328046cc33fae7462f8ee527f
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-18 18:51:05 +0530

    revert login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,14 @@
-import { signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
-import React, { useEffect, useState, useCallback } from "react";
+import {
+  signInWithEmailAndPassword,
+  signInWithPopup
+} from "firebase/auth";
+import React, { useEffect, useState, useCallback} from "react";
 import { Link, useNavigate } from "react-router-dom";
 import meeting2 from "../../assets/meeting2.png";
+
 import googlePlay from "../../assets/google-play.png";
 import hide from "../../assets/hide.png";
+// import meeting from "../../assets/meeting.png";
 import microsoft from "../../assets/microsoft.png";
 import show from "../../assets/show.png";
 import { auth, googleAuthProvider } from "../../firebase/auth";
@@ -12,6 +17,7 @@ import { FaSyncAlt, FaEnvelope, FaKey, FaShieldVirus } from "react-icons/fa";
 import validate from "../../common/validation";
 import Footer from "../Footer/Footer";
 import { ToastContainer, toast } from 'react-toastify';
+// import 'react-toastify/dist/ReactToastify.css';
 
 export default function Login() {
   const [error, setError] = useState({});
@@ -21,35 +27,34 @@ export default function Login() {
   const [loginInfo, setLoginInfo] = useState({
     email: "",
     password: "",
-  });
+  }); 
 
-  const handleLoginInfo = useCallback((e) => {
-    const { name, value } = e.target;
-    setLoginInfo((prev) => {
-      return { ...prev, [name]: value };
-    });
-
+  // Function for handling inputs
+  const handleLoginInfo = useCallback((e)=>{
+    const {name, value} = e.target;
+    setLoginInfo((prev)=>{
+      return {...prev, [name]: value}
+    })
     let errObj = validate[name](value);
-    if (name === "password") {
+    if(name === "password"){
       errObj = validate.loginPassword(value);
     }
-
-    setError((prev) => {
-      return { ...prev, ...errObj };
-    });
-  }, []);
+    setError((prev)=>{
+      return {...prev, ...errObj}
+    })
+  })
 
   const passwordToggle = useCallback(() => {
     if (passwordType === "password") {
       setPasswordType("text");
     } else setPasswordType("password");
-  }, [passwordType]);
+  });
 
   const navigate = useNavigate();
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
-        toast.success("Authenticating your credentials… 🚀", {
+        toast.success("Authenticating your credentials… 🚀",{
           className: "toast-message",
         });
         setTimeout(() => {
@@ -57,43 +62,44 @@ export default function Login() {
         }, 2000);
       }
     });
-  }, [navigate]);
+  }, []);
 
-  const generateCaptcha = useCallback(() => {
-    let captcha = "";
-    const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const generateCaptcha = useCallback(()=>
+    {
+      let captcha = "";
+      const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     for (let i = 0; i < 6; i++) {
       var randomIndex = Math.floor(Math.random() * charset.length);
       captcha += charset.charAt(randomIndex);
     }
+    setCaptchaText(captcha)
+    })
 
-    setCaptchaText(captcha);
-  }, []);
+    useEffect(()=>{
+      generateCaptcha();
+    }, [])
 
-  useEffect(() => {
-    generateCaptcha();
-  }, [generateCaptcha]);
-
+  // if signin with EmailId/password success then navigate to /dashboard
   const handleSignIn = useCallback((e) => {
-    e.preventDefault();
-    let submitable = true;
-    if (captchaVal !== captchaText) {
-      toast.error("Wrong Captcha", {
+     e.preventDefault();
+     let submitable = true;
+     if(captchaVal !== captchaText){
+      toast.error("Wrong Captcha",{
         className: "toast-message",
-      });
+      })
       setCaptchaVal("");
       generateCaptcha();
       return;
     }
 
-    Object.values(error).forEach((err) => {
-      if (err !== false) {
+    Object.values(error).forEach((err)=>{
+      if(err !== false){
         submitable = false;
         return;
       }
-    });
-    if (submitable) {
+    })
+    if(submitable){
       signInWithEmailAndPassword(auth, loginInfo.email, loginInfo.password)
         .then(() => {
           setTimeout(() => {
@@ -102,123 +108,135 @@ export default function Login() {
         })
         .catch((err) => {
           if (err.code === "auth/wrong-password") {
-            toast.error("Incorrect Password!", {
+            toast.error("Incorrect Password!",{
               className: "toast-message",
             });
           } else if (err.code === "auth/user-not-found") {
-            toast.error("This email is not registered", {
+            toast.error("This email is not registered",{
               className: "toast-message",
             });
           } else {
             console.error("Sign-in error", err);
-            toast.error("An error occurred. Please try again!", {
+            toast.error("An error occurred. Please try again!",{
               className: "toast-message",
             });
           }
         });
-    } else {
-      toast.error("Please fill all Fields with Valid Data.", {
-        className: "toast-message",
-      });
-    }
-  }, [captchaVal, captchaText, error, generateCaptcha, loginInfo.email, loginInfo.password, navigate]);
-
+      }else{
+        toast.error("Please fill all Fields with Valid Data.",{
+          className: "toast-message",
+        })
+      }
+  });
+  // Popup Google signin
   const SignInGoogle = useCallback(() => {
     signInWithPopup(auth, googleAuthProvider)
       .then(() => {
-        toast.success("Login successful !", {
+        toast.success("Login successful !",{
           className: "toast-message",
-        });
+        })
         setTimeout(() => {
           navigate("/dashboard");
         }, 2000);
       })
-      .catch((err) => toast.error(err.message, {
+      .catch((err) => toast.error(err.message,{
         className: "toast-message",
       }));
-  }, [navigate]);
+  });
 
   return (
     <main>
-      <div className="login-container">
-        <div className="parent">
-          <ToastContainer />
-          <div className="right" style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-            <h1 className="counsellor">Counsellor</h1>
-            <div className="sign-in">Log in to your account</div>
+    <div className="login-container">
+      <div className="parent">
+        {/* Home icon */}
+        {/* This is the right side of the login page   */}
+        <ToastContainer/>
+        <div className="right">
+          <h1 className="counsellor">Counsellor</h1>
+          <div className="sign-in">Log in to your account</div>
 
-            <form className="form" onSubmit={handleSignIn}>
-              <div>
-                <label htmlFor="email">Email</label>
+          {/* Login form */}
+          <form className="form" onSubmit={handleSignIn}>
+          <div>
+          <label htmlFor="email">Email</label>
+          <div className="iconContainer">
+          <input
+              id="email"
+              type="text"
+              name="email"
+              onChange={handleLoginInfo}
+              value={loginInfo.email}
+              placeholder="Email"
+              required
+              className={`${error.emailError && "inputField"}`}
+            />
+            <FaEnvelope className="icons"/>
+          </div>
+           
+            {error.email && error.emailError && <p className="errorShow">{error.emailError}</p>}
+
+          </div>
+             <div>
+             <label htmlFor="password">Password</label>
+            <div className="password-input">
+            <div className="iconContainer">
+            <input
+                id="password"
+                name="password"
+                type={passwordType}
+                onChange={handleLoginInfo}
+                value={loginInfo.password}
+                required
+                placeholder="Password"
+                className={`${error.passwordError && "inputField"}`}
+              />
+              <FaKey className="icons"/>
+              <div onClick={passwordToggle} className="toggle-button">
+                <img
+                  height={20}
+                  width={20}
+                  src={passwordType === "password" ? hide : show}
+                  alt="password-toggle"
+                />
+              </div>
+              {error.password && error.passwordError && <p className="errorShow">{error.passwordError}</p>}
+            </div>       
+            </div>
+             </div>
+            <div id="captcha-container">
+              <label htmlFor="captcha">Captcha</label>
+              <div
+                className="flex flex-row gap-3 justify-center items-center"
+                id="captchaBox"
+              >
+                <div id="captcha">{captchaText}</div>
+                <FaSyncAlt
+                id="captchaIcon"
+                  onClick={generateCaptcha}
+                />
                 <div className="iconContainer">
-                  <input
-                    id="email"
-                    type="text"
-                    name="email"
-                    onChange={handleLoginInfo}
-                    value={loginInfo.email}
-                    placeholder="Email"
-                    required
-                    className={`${error.emailError && "inputField"}`}
-                  />
-                  <FaEnvelope className="icons" />
+                <input
+                  type="text"
+                  name="captch"
+                  value={captchaVal}
+                  placeholder="Enter Captcha Here"
+                  onChange={(e) => setCaptchaVal(e.target.value)}
+                  className="w-[100%] bg-slate-100 py-2 px-4 focus:outline-indigo-500"
+                  required
+                />
+                <FaShieldVirus className="icons"/>
                 </div>
-
-                {error.email && error.emailError && (
-                  <p className="errorShow">{error.emailError}</p>
-                )}
+               
               </div>
-
-              <div>
-                <label htmlFor="password">Password</label>
-                <div className="iconContainer">
-                  <input
-                    id="password"
-                    type={passwordType}
-                    name="password"
-                    onChange={handleLoginInfo}
-                    value={loginInfo.password}
-                    placeholder="Password"
-                    required
-                    className={`${error.passwordError && "inputField"}`}
-                  />
-                  <FaKey className="icons" onClick={passwordToggle} />
-                </div>
-
-                {error.password && error.passwordError && (
-                  <p className="errorShow">{error.passwordError}</p>
-                )}
-              </div>
-
-              <div id="captcha-container">
-                <label htmlFor="captcha">Captcha</label>
-                <div className="flex flex-row gap-3 justify-center items-center" id="captchaBox">
-                  <div id="captcha">{captchaText}</div>
-                  <FaSyncAlt id="captchaIcon" onClick={generateCaptcha} />
-                  <div className="iconContainer">
-                    <input
-                      type="text"
-                      name="captch"
-                      value={captchaVal}
-                      placeholder="Enter Captcha Here"
-                      onChange={(e) => setCaptchaVal(e.target.value)}
-                      className="w-[100%] bg-slate-100 py-2 px-4 focus:outline-indigo-500"
-                      required
-                    />
-                    <FaShieldVirus className="icons" />
-                  </div>
-                </div>
-              </div>
-
-              <div className="remember-me">
-                <input type="checkbox" id="remember-me" />
-                <label htmlFor="remember-me"> Remember me</label>
-              </div>
+            </div>
+            <div className="remember-me">
+              <input type="checkbox" id="remember-me" />
+              <label htmlFor="remember-me"> Remember me</label>
+            </div>
               <button className="login_btn" type="submit">
                 Login
               </button>
-            </form>
-
+          </form>
             <div className="btn">
               <Link to="/forgotpassword" className="forgot-password">
                 Forgot Your password?
@@ -229,14 +247,26 @@ export default function Login() {
                 Don't have an account?
               </Link>
             </div>
-          </div>
-
-          <div className="left">
-            <img src={meeting2} alt="meeting" />
-          </div>
+            <div className="get-app">
+              {/* <div className="get-app-title">Get the app.</div> */}
+              {/* <div className="apps">
+                <Link to="/">
+                  <img className="microsoft-logo" src={microsoft} alt="" />
+                </Link>
+                <Link to="/">
+                  <img className="google-play-logo" src={googlePlay} alt="" />
+                </Link>
+              </div> */}
+            </div>
         </div>
-        <Footer />
+           {/* This is the left side of the login page   */}
+           <div className="left">
+           <img src={meeting2} alt="meeting" />
+          {/* <p className="left-text">Still Confused with College Choice?</p> */}
+        </div>
       </div>
+    </div>
+    <Footer />
     </main>
   );
-}
+};
