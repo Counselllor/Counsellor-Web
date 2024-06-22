5d4d6016fed76d7f566f491c843c61b1b27477d5
Sahil Ali <84167034+thestarsahil@users.noreply.github.com>
2024-06-22 16:34:49 +0530

    Revert "Added Dark Mode Toggle"

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,12 @@ import "./styles/App.css";
 
 import Dashboard from './components/Dashboard/Dashboard';
 import CollegePage from './components/CollegePage/CollegePage';
-import { createContext, useState } from 'react';
-
-//theme context
-export const ThemeContext = createContext(null);
-//App
 const App = () => {
 
-  const [theme, setTheme] = useState("light");
-
-  //Toggle theme
-  const toggleTheme = () => {
-    setTheme((curr) => (curr === "light" ? "dark" : "light"));
-  }
-
   return (
-    <ThemeContext.Provider value={{theme, toggleTheme}}>
-    <div className="App" id={theme}>
-      <Outlet />
+    <div className="App">
+      <Outlet Dashboard />
     </div>
-    </ThemeContext.Provider>
   );
 };
 
