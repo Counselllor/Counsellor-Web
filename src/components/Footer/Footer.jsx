2d7ebdd3394a21c4c9fad20d5e66c640b45d56eb
Damini2004 <divyachachane8@gmail.com>
2024-06-28 20:33:21 +0530

    Commit 1

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,18 @@ import { FaGithub, FaInstagram, FaFacebook, FaLinkedin } from "react-icons/fa";
 function Footer() {
   return (
     <footer>
+        <section class="newsletter-section">
+        <h2>Stay updated with our latest news!</h2>
+        <p>Subscribe to our newsletter to receive exclusive updates, promotions, and more.</p>
+        <form id="newsletter-form">
+            <input type="email" id="email" placeholder="Enter your email address"/>
+            <button id="subscribe-btn">Subscribe</button>
+        </form>
+        <div id="newsletter-response"></div>
+        </section>
       <div>
+    
+
         <ul>
           <li>
             <a
