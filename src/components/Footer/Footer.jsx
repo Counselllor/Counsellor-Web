909e2358cf40e6c2436e80a2ae1e0f74b1297565
AsmitaMishra24 <146121869+AsmitaMishra24@users.noreply.github.com>
2024-07-06 03:56:54 +0530

    Redesigned Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,33 +6,37 @@ const Footer = () => {
   return (
     <footer id="footer" className="footer-area">
       <div className="container">
-        <ul className="link">
-          <li><a href="/">Home</a></li>
-          <li><a href="./About">About Us</a></li>
-          <li><a href="./blogs">Blog</a></li>
-          <li><a href="./help">Help</a></li>
-          <li><a href="./contact">Contact Us</a></li>
-        </ul>
-      </div>
-      <div className="container footer-bottom">
-        <ul className="link-2">
-          <li><a href="./Terms">Terms & Conditions</a></li>
-          <li><a href="./privacy-policy">Privacy Policy</a></li>
-        </ul>
-        <div className="footer-copyright">
-          <p className="text">
-            <span className='line'></span>
-            © {new Date().getFullYear()} from Counsellor
-          </p>
+        <div className="link-container">
+          <ul className="link">
+            <li><a href="/">Home</a></li>
+            <li><a href="./About">About Us</a></li>
+            <li><a href="./blogs">Blog</a></li>
+            <li><a href="./contribute">Our Contributors</a></li>
+            <li><a href="./join-us">Join Us</a></li>
+            <li><a href="./help">Help</a></li>
+            <li><a href="./contact">Contact Us</a></li>
+          </ul>
         </div>
-        <ul className="social-icons">
-          <li><a href="https://github.com/Counselllor/Counsellor-Web" target="_blank" rel="noreferrer"><FaGithub /></a></li>
-          <li><a href="https://discord.com" target="_blank" rel="noreferrer"><FaDiscord /></a></li>
-          <li><a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><FaInstagram /></a></li>
-          <li><a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FaFacebook /></a></li>
-          <li><a href="https://www.linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
-          <li><a href="https://slack.com" target="_blank" rel="noreferrer"><FaSlack /></a></li>
-        </ul>
+        <div className="footer-bottom">
+          <ul className="link-2">
+            <li><a href="./Terms">Terms & Conditions</a></li>
+            <li><a href="./privacy-policy">Privacy Policy</a></li>
+          </ul>
+          <div className="footer-copyright">
+            <p className="text">
+              <span className='line'></span>
+              © {new Date().getFullYear()} from Counsellor
+            </p>
+          </div>
+          <ul className="social-icons">
+            <li><a href="https://github.com/Counselllor/Counsellor-Web" target="_blank" rel="noreferrer"><FaGithub /></a></li>
+            <li><a href="https://discord.com" target="_blank" rel="noreferrer"><FaDiscord /></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><FaInstagram /></a></li>
+            <li><a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><FaFacebook /></a></li>
+            <li><a href="https://www.linkedin.com" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
+            <li><a href="https://slack.com" target="_blank" rel="noreferrer"><FaSlack /></a></li>
+          </ul>
+        </div>
       </div>
     </footer>
   );
