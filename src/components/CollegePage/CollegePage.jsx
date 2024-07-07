a85fb2e183b1724ac5f70a44203806c9fa387a8e
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-07-07 22:53:20 +0530

    fetch and update college detailed page

--- src/components/CollegePage/CollegePage.jsx
+++ src/components/CollegePage/CollegePage.jsx
@@ -1,38 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import collegesData from '../Dashboard/colleges.json';
-import studentsData from './students.json';
-import './CollegePage.css';
-import { useParams, useNavigate } from 'react-router-dom';
-import Logo from '../../assets/logo.webp';
+import React, { useState, useEffect } from "react";
+import collegesData from "../Dashboard/colleges.json";
+import studentsData from "./students.json";
+import "./CollegePage.css";
+import { useParams, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import Navbar from "../Navbar/Navbar";
 import { auth } from "../../firebase/auth";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 import ScrollToTop from "react-scroll-to-top";
-import {FaStar,FaStarHalf} from 'react-icons/fa6'
-
+import { FaStar } from "react-icons/fa";
 
 const CollegePage = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   const navigate = useNavigate();
   const { id } = useParams();
-
-
-  const college = collegesData.find(college => college.id === parseInt(id));
-
+  const college = collegesData.find((college) => college.id === parseInt(id));
 
   if (!college) {
     return <div>College not found</div>;
   }
 
-  const [selectedCourse, setSelectedCourse] = useState('BTech');
-
-  const filteredStudents = studentsData.filter(student => student.course === selectedCourse && student.college === college.name);
-
-
-  const [menuOpen, setMenuOpen] = useState(false);
+  const [selectedCourse, setSelectedCourse] = useState("BTech");
+  const filteredStudents = studentsData.filter(
+    (student) =>
+      student.course === selectedCourse && student.college === college.name
+  );
 
   const handleSignOut = () => {
     signOut(auth)
@@ -44,103 +39,110 @@ const CollegePage = () => {
       });
   };
 
-  const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-  };
-
-  const imgArray=['/src/assets/9.png','/src/assets/8.png','/src/assets/10.png','/src/assets/11.png','/src/assets/element 6.png','/src/assets/element 7.png']
   return (
     <>
-      <ScrollToTop color='white' style={{ backgroundColor: "#5CB6F9" }} />
-      <Navbar/>
-      {/* <nav className="navbar">
-        <div className="logo">
-          <img src={Logo} alt="Logo" />
-        </div>
-        <div className={`menu ${menuOpen ? 'show' : ''}`}>
-          <ul>
-            <li><a href="#">Top Universities</a></li>
-            <li><a href="#">Jobs</a></li>
-            <li><a href="#">Courses</a></li>
-            <li><a href="#">Career Support</a></li>
-            <li className='dot'><a href="#">•</a></li>
-            <li><a href="#" onClick={handleSignOut}>Log Out</a></li>
-            <li><a href="#"><button className='profile_btn'>Profile</button></a></li>
-          </ul>
-        </div>
-        <div className="hamburger" onClick={toggleMenu}>
-          <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
-          <div className={`bar ${menuOpen ? 'open' : ''}`}></div>
-        </div>
-      </nav> */}
-      <div style={{height:"60px"}}></div>
-      <div className="page">
-        <div className="left">
-          <div className="content">
-            <div style={{height:"auto",width:"100%",paddingTop:"20px"}}>
-
-            <h1 className="college-name abeezee-regular">{college.name}</h1>
-            <div className="description-card">
-              <p className="description clipped-text abeezee-regular">{college.description}</p>
-            </div>
-            <div className="group" >
-              <div className="location">
-                <div className='icon'>
-                  <Icon icon="ion:location-outline" style={{ fontSize: '24px'}} />
-                </div>
-                <div className="location-depth">
-                  <p className="location-heading abeezee-regular">Location</p>
-                <p className="location-text clipped-text1 abeezee-regular">{college['exact-location']}</p>
-                </div>
-                </div>
-              <div className="rating">
-                <div >
-        <FaStar /><FaStar/> <FaStar/><FaStar/><FaStar/>
-                </div>
-                <div className='rate'>
-
-                <p className="rating-heading abeezee-regular">Rating</p>
-                <p className="rating-text abeezee-regular" >{college.rating}/10</p>
-                </div>
+      <ScrollToTop color="white" style={{ backgroundColor: "#5CB6F9" }} />
+      <Navbar />
+      <div className="college-page-container">
+        <div className="college-info">
+          <h1 className="college-name">{college.name}</h1>
+          <p className="college-description">{college.description}</p>
+          <div className="college-details">
+            <div className="college-location">
+              <Icon icon="ion:location-outline" className="icon" />
+              <div>
+                <p className="detail-heading">Location</p>
+                <p className="detail-text">{college["exact-location"]}</p>
               </div>
-                <button className="search-button "><a href={college.website} target="_blank" rel="noreferrer">Search</a></button>
             </div>
-            </div>
-            <img className="image" src={college.imageURL} />
-          </div>
-        </div>
-        <div className="right">
-          <div className="searchCourses" style={{marginTop:"20px"}}>
-
-            <select id="courseList" value={selectedCourse} placeholder="Search courses" style={{backgroundColor:"#c5d5d4",outline:"none",border:"none",fontWeight:"600",fontSize:"20px"}} onChange={(e) => setSelectedCourse(e.target.value)}>
-              <option  value="BTech">BTech</option>
-              <option value="BBA">BBA</option>
-              <option value="MBA">MBA</option>
-              <option value="BCA">BCA</option>
-              <option value="BSc">BSc</option>
-              <option value="MSc">MSc</option>
-              <option value="PHD">PHD</option>
-            </select>
-          </div>
-
-            {filteredStudents.map((student,index) => (
-              <div key={student.id} className="student-card">
-                <div style={{width:"20%",display: "flex", paddingLeft:"20px", alignitems: "center"}}><img src={`${imgArray[index]}`} style={{height:"80%",minHeight:"66px",maxWidth:"80%"}}></img></div>
-                <div className='st'>
-                <p className='abeezee-regular' style={{textAlign:"left",width:"50%",fontSize:"15px",marginBottom:"0px",marginTop:"10px",paddingLeft:"8px",backgroundColor:"rgba(255,255,255,0.3)",borderRadius:"15px"}}>{student.name}</p>
-                <div className="student-description">
-                  <p className='abeezee-regular' style={{marginBottom:"0px",marginTop:"18px",fontSize:"20px",fontWeight:"600"}}>{student.course}</p>
-                  <p className='abeezee-regular' style={{marginBottom:"0px",marginTop:"18px",fontSize:"20px",fontWeight:"600"}}>{student.branch}</p>
-                  <p className='abeezee-regular' style={{marginBottom:"0px",marginTop:"18px",fontSize:"20px",fontWeight:"600"}}>{student.year}</p>
-                </div>
-                <p className="position abeezee-regular">{student.position}</p>
-                </div>
+            <div className="college-rating">
+              <div className="stars">
+                <FaStar size={"20px"} />
               </div>
-            ))}
+              <div>
+                <p className="detail-heading">Rating</p>
+                <p className="detail-text">{college.rating}/10</p>
+              </div>
+            </div>
+            <a
+              href={college.website}
+              target="_blank"
+              rel="noreferrer"
+              className="college-website-button"
+            >
+              Visit Website
+            </a>
+          </div>
+          <img
+            src={college.imageURL}
+            alt={college.name}
+            className="college-image"
+          />
+        </div>
+        <div className="students-section">
+          <h1 className="college-name">Student Section</h1>
+          <select
+            className="course-select"
+            value={selectedCourse}
+            onChange={(e) => setSelectedCourse(e.target.value)}
+          >
+            <option value="BTech">BTech</option>
+            <option value="BBA">BBA</option>
+            <option value="MBA">MBA</option>
+            <option value="BCA">BCA</option>
+            <option value="BSc">BSc</option>
+            <option value="MSc">MSc</option>
+            <option value="PHD">PHD</option>
+          </select>
+          {filteredStudents.map((student, index) => (
+            <div key={student.id} className="student-card">
+              <img
+                src={`https://cdn-icons-png.flaticon.com/512/2784/2784461.png`}
+                alt={student.name}
+                className="student-photo"
+              />
+              <div className="student-info">
+                <p className="student-name">{student.name}</p>
+                <p className="student-course">{student.course}</p>
+                <p className="student-branch">{student.branch}</p>
+                <p className="student-year">{student.year}</p>
+                <p className="student-position">{student.position}</p>
+              </div>
+            </div>
+          ))}
+
+          <div className="cards">
+            <div className="card1">
+              <div className="card-content">
+                <i className="fas fa-trophy fa-2x"></i>
+                <h1>Achievements</h1>
+              </div>
+            </div>
+            <div className="card2">
+              <div className="card-content">
+                <i className="fas fa-book fa-2x"></i>
+                <h1>Books</h1>
+              </div>
+            </div>
+            <div className="card3">
+              <div className="card-content">
+                <i className="fas fa-football-ball fa-2x"></i>
+                <h1>Sports</h1>
+              </div>
+            </div>
+            <div className="card4">
+              <div className="card-content">
+                <i className="far fa-clock fa-2x"></i>
+                <h1>TimeTable</h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="upcoming">
+            <h1>upcoming events</h1>
+          </div>
         </div>
       </div>
-
     </>
   );
 };
