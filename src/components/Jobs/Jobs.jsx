04b6818b185b21a8a7d33e68d03b134f58b47934
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-07-07 22:38:00 +0530

    update class name

--- src/components/Jobs/Jobs.jsx
+++ src/components/Jobs/Jobs.jsx
@@ -8,25 +8,25 @@ const Jobs = () => {
   return (
     <div>
       <Navbar />
-      <div className="jobs-container">
-        <header className="jobs-header">
+      <div className="jobsPage-container">
+        <header className="jobsPage-header">
           <h1>Available Jobs</h1>
         </header>
-        <div className="job-list">
+        <div className="jobsPage-list">
           {JobsData.map((job) => (
-            <div key={job.id} className="job-card">
-              <h2 className="job-title">{job.title}</h2>
-              <h3 className="job-company">{job.company}</h3>
-              <p className="job-location">{job.location}</p>
-              <p className="job-salary">Salary: <span style={{fontWeight: 600}}>{job.salary}</span> </p>
-              <p className="job-description">{job.description}</p>
-              <p className="job-requirements">
+            <div key={job.id} className="jobsPage-card">
+              <h2 className="jobsPage-title">{job.title}</h2>
+              <h3 className="jobsPage-company">{job.company}</h3>
+              <p className="jobsPage-location">{job.location}</p>
+              <p className="jobsPage-salary">Salary: <span style={{ fontWeight: 600 }}>{job.salary}</span> </p>
+              <p className="jobsPage-description">{job.description}</p>
+              <p className="jobsPage-requirements">
                 <strong>Requirements:</strong> {job.requirements}
               </p>
-              <p className="job-benefits">
+              <p className="jobsPage-benefits">
                 <strong>Benefits:</strong> {job.benefits}
               </p>
-              <p className="job-company-description">
+              <p className="jobsPage-companyDescription">
                 <strong>About Company:</strong> {job.companyDescription}
               </p>
             </div>
