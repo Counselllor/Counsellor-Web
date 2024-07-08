c08027e2256129ce46df3d3bc263fec6686c2368
Ayushmaanagarwal1121 <agarwalayushmaan88@gmail.com>
2024-07-08 20:01:34 +0530

    Fixed chanegs

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -7,6 +7,7 @@ import { ThemeContext } from '../../App';
 import { Switch } from 'antd';
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../firebase/auth";
+import { signOut } from "firebase/auth";
 
 const blogsData = [
   {
@@ -137,7 +138,7 @@ let [isLoggedIn,setLogin]=useState(false)
           className: "toast-message",
         });
       });
-  });
+  }, [navigate]);
 
   const toggleMenu = useCallback(() => {
     setMenuOpen(!menuOpen);
