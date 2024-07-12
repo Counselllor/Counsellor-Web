5f7193e9060a661570b4a80c076bd212cc4c9f19
Ayush Deb <ayushdeb848@gmail.com>
2024-07-12 22:18:28 +0530

    added Download data option

--- src/components/ProfileComponent/ProfileCard.jsx
+++ src/components/ProfileComponent/ProfileCard.jsx
@@ -80,6 +80,33 @@ const ProfileCard = () => {
     }
   };
 
+  const generateProfileData = () => {
+    return JSON.stringify({
+      name,
+      dob,
+      academicYear,
+      selectedSkills,
+      socialProfiles,
+      email: "counsellor@gmail.com",
+      phone: "+918795768574",
+      gender: "Male",
+      college: "IIT Bombay",
+    }, null, 2);
+  };
+
+  const downloadProfileData = () => {
+    const data = generateProfileData();
+    const blob = new Blob([data], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "profile_details.json";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div className="profile-card-container">
       <div className="greeting">
@@ -164,6 +191,9 @@ const ProfileCard = () => {
               ))}
             </ul>
           </div>
+          <button onClick={downloadProfileData} className="download-button">
+            Download Profile Details
+          </button>
         </div>
       </div>
       {isEditing && (
@@ -195,7 +225,6 @@ const ProfileCard = () => {
               />
             </label>
             
-            {/* Image upload moved here */}
             <div className="image-upload">
               <h3>Upload Profile Picture:</h3>
               <input type="file" accept="image/*" onChange={handleImageUpload} />
