340c5f40db55594fa61be31f1b4d178281ebd619
Mastan Sayyad <sayyadmastan78@gmail.com>
2024-07-14 05:25:32 +0530

    Update Privacy.jsx

--- src/components/Privacy-Policy/Privacy.jsx
+++ src/components/Privacy-Policy/Privacy.jsx
@@ -7,6 +7,9 @@ import BackToHomeButton from "../backtohome";
 const Breadcrumb = () => {
   return (
     <nav className="breadcrumb">
+      <Link to="/" className="breadcrumb-item">Counsellor</Link>
+      <span className="breadcrumb-separator">/</span>
+      <span className="breadcrumb-item active">Privacy Policy</span>
     </nav>
   );
 };
@@ -14,7 +17,7 @@ const Breadcrumb = () => {
 const Privacy = () => {
   return (
     <div>
-        <BackToHomeButton />
+      <BackToHomeButton />
       <div className="privacy-policy-container">
         <Breadcrumb />
         <div className="privacy-policy-content">
@@ -23,10 +26,10 @@ const Privacy = () => {
             Last updated:<span style={{ color: "blue" }}>29/05/2024</span>
           </p>
           <p>
-            This Privacy Policy describes Our policies and procedures on the
-            collection, use, and disclosure of Your information when You use the
-            Service and tells You about Your privacy rights and how the law
-            protects You.
+            This Privacy Policy describes our policies and procedures on the
+            collection, use, and disclosure of your information when you use the
+            service and tells you about your privacy rights and how the law
+            protects you.
           </p>
           <h2 className="title">Interpretation and Definitions</h2>
           <h3 className="sub-title">Interpretation</h3>
@@ -38,21 +41,20 @@ const Privacy = () => {
           </p>
           <h3 className="sub-title">Definitions</h3>
           <p>For the purposes of this Privacy Policy:</p>
-          <div className="definitions-list">
-          <ul>
+          <ul className="definitions-list">
             <li>
-              <strong>Account</strong> means a unique account created for You to
-              access our Service or parts of our Service.
+              <strong>Account</strong> means a unique account created for you to
+              access our service or parts of our service.
             </li>
             <li>
               <strong>Company</strong> (referred to as either "the Company",
-              "We", "Us" or "Our" in this Agreement) refers to [College Name],
+              "We", "Us" or "Our" in this Agreement) refers to Counsellor,
               [College Address].
             </li>
             <li>
-              <strong>Cookies</strong> are small files that are placed on Your
+              <strong>Cookies</strong> are small files that are placed on your
               computer, mobile device, or any other device by a website,
-              containing the details of Your browsing history on that website
+              containing the details of your browsing history on that website
               among its many uses.
             </li>
             <li>
@@ -60,21 +62,20 @@ const Privacy = () => {
             </li>
             <li>
               <strong>Device</strong> means any device that can access the
-              Service such as a computer, a cellphone, or a digital tablet.
+              service such as a computer, a cellphone, or a digital tablet.
             </li>
             <li>
               <strong>Personal Data</strong> is any information that relates to
               an identified or identifiable individual.
             </li>
           </ul>
-          </div>
           <h2 className="title">Collecting and Using Your Personal Data</h2>
           <h3 className="sub-title">Types of Data Collected</h3>
           <h4 className="sub-title">Personal Data</h4>
           <p>
-            While using Our Service, We may ask You to provide Us with certain
+            While using our service, we may ask you to provide us with certain
             personally identifiable information that can be used to contact or
-            identify You. Personally identifiable information may include, but
+            identify you. Personally identifiable information may include, but
             is not limited to:
           </p>
           <ul className="personal-data">
@@ -82,27 +83,24 @@ const Privacy = () => {
             <li>First name and last name</li>
             <li>Phone number</li>
             <li>Address, State, Province, ZIP/Postal code, City</li>
-            <li>Usage Data</li>
           </ul>
-          <h4 className="sub-title">Usage Data</h4>
-          <p>
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ullam consequatur rem ipsa maiores recusandae libero illo repellendus, excepturi necessitatibus?
-          </p>
-          <p>
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ullam consequatur rem ipsa maiores recusandae libero illo repellendus, excepturi necessitatibus?
-          </p>
-          <p>
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ullam consequatur rem ipsa maiores recusandae libero illo repellendus, excepturi necessitatibus?
-          </p>
-          <p>
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ullam consequatur rem ipsa maiores recusandae libero illo repellendus, excepturi necessitatibus?
-          </p>
-          <p>
-           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deserunt ullam consequatur rem ipsa maiores recusandae libero illo repellendus, excepturi necessitatibus?
-          </p>
-        </div>
-      </div>
 
+          <h3 className="sub-title">Usage of Your Personal Data</h3>
+          <p>The Company may use Personal Data for the following purposes:</p>
+          <ul className="usage-data">
+            <li><strong>To provide and maintain our Service</strong>, including to monitor the usage of our Service.</li>
+            <li><strong>To manage Your Account:</strong> to manage Your registration as a user of the Service. The Personal Data You provide can give You access to different functionalities of the Service that are available to You as a registered user.</li>
+            <li><strong>For the performance of a contract:</strong> the development, compliance, and undertaking of the purchase contract for the products, items, or services You have purchased or of any other contract with Us through the Service.</li>
+            <li><strong>To contact You:</strong> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products, or contracted services, including security updates, when necessary or reasonable for their implementation.</li>
+            <li><strong>To provide You</strong> with news, special offers </li>
+           </ul>
+
+           <h3 className="sub-title">Contact Us</h3>
+          <p>
+          If you have any questions or comments about this policy, you may email us at <a href="mailto:counsellorweb@support.com">counsellorweb@support.com</a>
+          </p>
+      </div>
+      </div>
       <hr />
 
       <Footer />
