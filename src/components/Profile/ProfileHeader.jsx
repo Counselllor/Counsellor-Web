26dc802826f1bac9ac1381a98b9e83c20ac45771
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-07-21 18:22:27 +0530

    fixed

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, {  useEffect, useState } from "react";
 import "./Profile.css";
 import { Link, useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
