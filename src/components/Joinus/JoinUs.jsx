639bdc43059ffcdfe23a2437a1eff8d6385d7367
Komal Agarwal <komal.agarwal5115@gmail.com>
2024-07-21 21:41:09 +0530

    fixed

--- src/components/Joinus/JoinUs.jsx
+++ src/components/Joinus/JoinUs.jsx
@@ -38,12 +38,12 @@ let [isLoggedIn,setLogin]=useState(false)
   return (
     <>
       <Navbar />
-      <BackToHomeButton />
+      {/* <BackToHomeButton /> */}
       <div>
         <div className="join-us-container">
           <div className="contents">
             <h1>Join Us</h1>
-            <section id="benefits">
+            <section id="benefitsnew" className="joinnew">
               <h2>Why Join Our Team?</h2>
               <p>
                 At CounsellorsWeb, we are dedicated to fostering a work
@@ -51,18 +51,76 @@ let [isLoggedIn,setLogin]=useState(false)
                 professional growth. Here are just a few reasons why you should
                 consider joining our team:
               </p>
-              <ul>
-                <li>Innovative projects and cutting-edge technology</li>
-                <li>Collaborative and inclusive work environment</li>
-                <li>Opportunities for professional growth</li>
-                <li>Competitive salary and benefits</li>
-                <li>Flexible working hours and remote work options</li>
-                <li>Health and wellness programs</li>
-                <li>Ongoing training and development</li>
-              </ul>
+              <div className="whynew">
+                <div className="whynewbox">
+                  <h3>🌟Innovative projects and cutting-edge technology</h3>
+                  <p>
+                    Engage in pioneering projects that utilize the latest
+                    technologies, pushing the boundaries of innovation. Be at
+                    the forefront of technological advancements, creating
+                    impactful solutions.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>🌍 Collaborative and inclusive work environment</h3>
+                  <p>
+                    Join a team that values diversity and inclusivity, fostering
+                    a culture of collaboration. Work together in a supportive
+                    environment where every voice is heard and respected.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>📈 Opportunities for professional growth</h3>
+                  <p>
+                    Access numerous opportunities for career advancement and
+                    skill development. Grow professionally through mentorship,
+                    challenging projects, and continuous learning.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>🎁 Competitive salaries and benefits</h3>
+                  <p>
+                    Enjoy a competitive salary package along with comprehensive
+                    benefits. Experience financial stability and rewards that
+                    reflect your hard work and dedication in your journey.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>⏰ Flexible working hours and remote work options</h3>
+                  <p>
+                    Benefit from flexible working hours and the option to work
+                    remotely. Achieve a healthy work-life balance while
+                    maintaining productivity and job satisfaction throughout.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>🏃‍♀️Health and wellness programs</h3>
+                  <p>
+                    Take advantage of health and wellness programs designed to
+                    support your physical and mental well-being. Participate in
+                    activities and initiatives that promote a healthy lifestyle.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>📚 Ongoing training and development</h3>
+                  <p>
+                    Engage in continuous training and development programs.
+                    Enhance your skills and knowledge through workshops,
+                    courses, and certifications tailored to your career goals.
+                  </p>
+                </div>
+                <div className="whynewbox">
+                  <h3>🏆 Employee recognition and rewards programs</h3>
+                  <p>
+                    Be acknowledged for your hard work and achievements through
+                    structured recognition and rewards programs. Enjoy a culture
+                    that celebrates successes and motivates employees with
+                    meaningful incentives.
+                  </p>
+                </div>
+              </div>
             </section>
-
-            <section id="culture">
+            <section id="culturenew">
               <h2>Our Culture</h2>
               <p>
                 Our company culture is built on mutual respect, continuous
@@ -71,34 +129,28 @@ let [isLoggedIn,setLogin]=useState(false)
                 and job satisfaction. Here’s what you can expect when you join
                 us:
               </p>
-              <ul>
-                <li>Regular team-building activities and events</li>
-                <li>Open-door policy with management</li>
-                <li>Encouragement of work-life balance</li>
-                <li>Diversity and inclusion initiatives</li>
-                <li>Community involvement and social responsibility</li>
-              </ul>
+              <ol className="cultin">
+                <li className="text">Regular team - building activities</li>
+                <li className="text">Open-door policy and management</li>
+                <li className="text">Encouraging of a work-life balance</li>
+                <li className="text">Diversity and inclusion initiatives do</li>
+                <li className="text">
+                  Community involvement and social responsibility
+                </li>
+                <li className="text">
+                  Opportunities for career growth and development
+                </li>
+              </ol>
             </section>
-
-            <section id="testimonials">
+            <section id="testimonialsnew">
               <h2>Employee Testimonials</h2>
               <p>
                 Don’t just take our word for it. Here’s what some of our
                 employees have to say about working at CounsellorsWeb:
               </p>
-              <blockquote>
-                "Working at CounsellorsWeb has been an incredible journey. The
-                collaborative environment and the focus on innovation make every
-                day exciting." – Jane Doe, Software Engineer
-              </blockquote>
-              <blockquote>
-                "The emphasis on continuous learning and professional
-                development has helped me grow both personally and
-                professionally." – John Smith, Project Manager
-              </blockquote>
+              <JoinStories />
             </section>
-
-            <section id="positions">
+            <section id="positionsnew">
               <h2>Open Positions</h2>
               <p>
                 We are always looking for talented individuals to join our team.
@@ -106,8 +158,7 @@ let [isLoggedIn,setLogin]=useState(false)
                 current openings.
               </p>
             </section>
-
-            <section id="apply">
+            <section id="applynew">
               <h2>How to Apply</h2>
               <p>
                 Interested in joining us? Send your resume and a cover letter to{" "}
@@ -116,18 +167,18 @@ let [isLoggedIn,setLogin]=useState(false)
                 </a>
                 . In your cover letter, please include:
               </p>
-              <ul>
-                <li>A brief introduction about yourself</li>
-                <li>Why you are interested in joining CounsellorsWeb</li>
-                <li>What you can bring to our team</li>
-                <li>Your relevant skills and experience</li>
-              </ul>
+              <ol className="cultin">
+                <li className="text">A brief introduction about yourself</li>
+                <li className="text">
+                  Why you are interested in joining Counsellors Web
+                </li>
+                <li className="text">What you can bring to our team</li>
+                <li className="text">Your relevant skills and experience</li>
+              </ol>
             </section>
           </div>
         </div>
-
         <hr />
-
         <Footer />
       </div>
     </>
