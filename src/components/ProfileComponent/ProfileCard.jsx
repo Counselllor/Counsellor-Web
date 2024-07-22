f30bc22475234a998b40cea8883374d09a69207c
anwesha <ANWESHA@DESKTOP-ALJFO4P.com>
2024-07-22 21:00:51 +0530

    fix:self_closing_tag_error_fixed

--- src/components/ProfileComponent/ProfileCard.jsx
+++ src/components/ProfileComponent/ProfileCard.jsx
@@ -311,7 +311,7 @@ const ProfileCard = () => {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <i className={`bx bxl-${userInfo.firstname?.toLowerCase()}`}></i>
+              <i className={`bx bxl-${userInfo.firstname?.toLowerCase()}`}/>
             </a>
           </div>
           <div className="skills-section">
