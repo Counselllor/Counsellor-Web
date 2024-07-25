8768dcc7a05c9b326a528a1440d57ad7130dae73
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-07-25 09:26:35 +0530

    update social media page style

--- src/components/Student/SoicalLinks.jsx
+++ src/components/Student/SoicalLinks.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import StudenData from '../CollegePage/students.json';
-import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
-import './SocialLinks.css';
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import StudenData from "../CollegePage/students.json";
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import "./SocialLinks.css";
 
 const SocialLinks = () => {
   const { id } = useParams();
-  const student = StudenData.find(student => student.id === parseInt(id));
+  const student = StudenData.find((student) => student.id === parseInt(id));
 
   if (!student) {
     return <div>Student not found</div>;
@@ -15,18 +15,56 @@ const SocialLinks = () => {
   return (
     <div className="social-links-container">
       <h2>{student.name}</h2>
-      <p>{student.position} at {student.college}</p>
+      <p>
+        {student.position} at {student.college}
+      </p>
       <hr />
       <div className="social-links">
-        <a href={student.socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
-          <FaLinkedin size={30} />
-        </a>
-        <a href={student.socialLinks.github} target="_blank" rel="noopener noreferrer">
-          <FaGithub size={30} />
-        </a>
-        <a href={student.socialLinks.twitter} target="_blank" rel="noopener noreferrer">
-          <FaTwitter size={30} />
-        </a>
+        <Link
+          to={student.socialLinks.linkedin}
+          style={{ textDecoration: "none" }}
+        >
+          <div className="linked-in">
+            <a
+              href={student.socialLinks.linkedin}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaLinkedin size={30} />
+            </a>
+            <h1>LinkedIn</h1>
+          </div>
+        </Link>
+        <Link
+          to={student.socialLinks.github}
+          style={{ textDecoration: "none" }}
+        >
+          <div className="github">
+            <a
+              href={student.socialLinks.github}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaGithub size={30} />
+            </a>
+            <h1>Github</h1>
+          </div>
+        </Link>
+        <Link
+          to={student.socialLinks.twitter}
+          style={{ textDecoration: "none" }}
+        >
+          <div className="twitter">
+            <a
+              href={student.socialLinks.twitter}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FaTwitter size={30} />
+            </a>
+            <h1>Twitter</h1>
+          </div>
+        </Link>
       </div>
     </div>
   );
