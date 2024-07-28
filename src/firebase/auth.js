5a722798979b49f046f23ebb3d34bcfe3c2587ab
anwesha <ANWESHA@DESKTOP-ALJFO4P.com>
2024-07-28 11:44:24 +0530

    refac:pulled and pushed again

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
   messagingSenderId: "1055143292636",
   appId: "1:1055143292636:web:1397f7974a27bd486e0e19",
   measurementId: "G-MJST0MPDZ3"
-
 };
 
 // Initialize Firebase
